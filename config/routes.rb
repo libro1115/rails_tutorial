b9fc0bd1df0f6c9512b7ae2d4a5278be9146e7ce
libro1115 <libro.1115@gmail.com>
2022-10-11 20:39:14 +0900

    application routes draw

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/books",to:"books#index"
-  post "/books",to:"books#create"
-  get "/books/new",to:"books#new",as:"new_book"
-  get "/books/:id/edit",to:"books#edit",as:"edit_book"
-  get "/books/:id",to:"books#show",as:"book"
-  patch "/books/:id",to:"books#update"
-  delete "/books/:id",to:"books#destroy"
+  Rails.application.routes.draw do
+    resources :books
+  end
+  # get "/books",to:"books#index"
+  # post "/books",to:"books#create"
+  # get "/books/new",to:"books#new",as:"new_book"
+  # get "/books/:id/edit",to:"books#edit",as:"edit_book"
+  # get "/books/:id",to:"books#show",as:"book"
+  # patch "/books/:id",to:"books#update"
+  # delete "/books/:id",to:"books#destroy"
 end
