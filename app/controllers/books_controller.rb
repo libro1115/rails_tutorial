e8a51bfa6bfda92db283a090671937652e59458f
libro1115 <libro.1115@gmail.com>
2022-10-11 19:47:02 +0900

    メッセージ表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,10 +17,12 @@ class BooksController < ApplicationController
         @book = Book.new(book_params)
         respond_to do |format|
             if @book.save
-                format.html{redirect_to books_path(@book),notice:"Book was successfully created"}
+                flash[:notice]="データ登録に成功"
+                format.html{redirect_to books_path(@book),notice:"1件のデータ登録"}
                 format.json{render:show, status: :created, location: @book}
             else
-                format.html{render:new, status: :unprocessable_entity}
+                flash[:error]="データ登録に失敗"
+                format.html{render:new, error:"データ登録に失敗", status: :unprocessable_entity}
                 format.json{render json:@book.errors, status: :unprocessable_entity}
             end
         end
@@ -34,8 +36,10 @@ class BooksController < ApplicationController
         @book = Book.find(params[:id])
         book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
         if @book.update(book_params)
-                redirect_to books_path
+            flash[:notice]="データを1件更新"
+            redirect_to books_path
         else 
+            flash.now[:error]="データの更新に失敗"
             render :edit
         end
     end
@@ -44,6 +48,7 @@ class BooksController < ApplicationController
         @book = Book.find(params[:id])
         @book.destroy
         redirect_to do |f|
+            flash[:notice]="1件のデータを削除"
             f.html{redirect_to books_path, notice:"Task was destoryed",status: :see_other}
             f.json{head:no_content}
         end
