936ef237007b1b798bedca5e3241b17924b9d047
libro1115 <libro.1115@gmail.com>
2022-10-11 17:31:41 +0900

    新規登録、更新、削除

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,50 @@ class BooksController < ApplicationController
     def index
         @books = Book.all
     end
+
     def show
         @book = Book.find(params[:id])
+    end
+
+    def new
+        @book = Book.new
+        @book.year = 2022
+    end
+
+    def create
+        book_params = params.require(:book).permit(:year, :month,:inout,:category,:amount)
+        @book = Book.new(book_params)
+        respond_to do |format|
+            if @book.save
+                format.html{redirect_to books_path(@book),notice:"Book was successfully created"}
+                format.json{render:show, status: :created, location: @book}
+            else
+                format.html{render:new, status: :unprocessable_entity}
+                format.json{render json:@book.errors, status: :unprocessable_entity}
+            end
+        end
+    end
+
+    def edit
+        @book = Book.find(params[:id])
+    end
+
+    def update
+        @book = Book.find(params[:id])
+        book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
+        if @book.update(book_params)
+                redirect_to books_path
+        else 
+            render :edit
+        end
+    end
+    
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to do |f|
+            f.html{redirect_to books_path, notice:"Task was destoryed",status: :see_other}
+            f.json{head:no_content}
+        end
     end
 end
